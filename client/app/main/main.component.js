bc603e988f790ab51ae782074bff9f28513a6ae5
Kla Haeck <kla.haeck@gmail.com>
2018-09-26 10:57:41 -0500

    updates

--- client/app/main/main.component.js
+++ client/app/main/main.component.js
@@ -18,35 +18,40 @@ export class MainController {
   }
 
   upload(file) {
-    this.submitted = true;
-    this.Upload.upload({
-      url: '/api/submissions',
-      data: { file }
-    })
-    .then(() => {
-      this.msg = {
-        class: 'success',
-        text: 'Your photo has been uploaded!'
-      };
-      // console.log(`Success ${resp.config.data.file.name} uploaded. Response: ${resp.data}`);
-    }, resp => {
-      console.log(`Error status: ${resp.status}`);
-      this.submitted = false;
-      this.msg = {
-        class: 'danger',
-        text: resp.status
-      };
-    }, evt => {
-      this.progressPercentage = parseInt(100.0 * evt.loaded / evt.total, 10);
-      // console.log(`progress: ${progressPercentage}% ${evt.config.data.file.name}`);
-    })
-    .catch(resp => {
-      this.submitted = false;
-      this.msg = {
-        class: 'danger',
-        text: resp.status
-      };
-    });
+    if (file) {
+      this.submitted = true;
+      this.Upload.upload({
+        url: '/api/submissions',
+        data: { file }
+      })
+      .then(resp => {
+        console.log(resp.data);
+        this.backgroundImage = `${resp.data.file.path}/${resp.data.file.versions.lines}`;
+        // this.msg = {
+        //   class: 'success',
+        //   text: 'Your photo has been uploaded!'
+        // };
+        this.submitted = false;
+        // console.log(`Success ${resp.config.data.file.name} uploaded. Response: ${resp.data}`);
+      }, resp => {
+        console.log(`Error status: ${resp.status}`);
+        this.submitted = false;
+        this.msg = {
+          class: 'danger',
+          text: resp.status
+        };
+      }, evt => {
+        this.progressPercentage = parseInt(100.0 * evt.loaded / evt.total, 10);
+        // console.log(`progress: ${progressPercentage}% ${evt.config.data.file.name}`);
+      })
+      .catch(resp => {
+        this.submitted = false;
+        this.msg = {
+          class: 'danger',
+          text: resp.status
+        };
+      });
+    }
   }
 }
 
